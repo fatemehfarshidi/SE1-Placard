490b21aff0b515d6530c10df1df46e0fc7697db3
fatemehfarshidi <Fati.farshidi@gmail.com>
2022-12-11 23:11:19 +0330

    [add] Dockerfile, docker-compose

---
+++ brick/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
