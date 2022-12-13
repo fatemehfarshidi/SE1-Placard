a49a9db564632c32d8825816508991dc30e3e777
saya <sayahashemian@gmail.com>
2022-12-13 14:37:23 +0330

    model updated

--- brick/models.py
+++ brick/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
 
-# Create your models here.
+class Post(models.Model):
+    TYPE_BORROW = 'B'
+    TYPE_SELL = 'S'
 
+    TYPE_CHOICES = [
+        (TYPE_BORROW, 'Borrow'),
+        (TYPE_SELL, 'Sell'),
+    ]
+
+
+    title = models.CharField(max_length=255)
+    user_id = models.PositiveIntegerField()
+    description = models.TextField(null=True, blank=True)
+    type = models.CharField(
+        max_length=1, choices=TYPE_CHOICES, default=TYPE_SELL)
+        
