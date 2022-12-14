850061af2fb9f3f27f3fdfce13b21ea872256269
alighasemi01 <alighasemic137@gmail.com>
2022-12-14 06:44:56 +0330

    added log-in panel

--- placard/urls.py
+++ placard/urls.py
@@ -1,18 +1,3 @@
-"""placard URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from brick import views
@@ -21,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("brick/", include("brick.urls")),
     path("register/", views.register_request, name="register"),
+    path.("login/", views.login_request, name="login"),
 ]
