3a8a46a53fe87a2147697f6104bceb184eea65af
saya <sayahashemian@gmail.com>
2022-12-16 03:10:15 +0330

    CRUD

--- brick/views.py
+++ brick/views.py
@@ -4,6 +4,11 @@ from .forms import NewUserForm
 from django.contrib.auth import login, authenticate #add this
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm #add this
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -42,3 +47,35 @@ def login_request(request):
 			messages.error(request,"Invalid username or password.")
 	form = AuthenticationForm()
 	return render(request=request, template_name="login/login.html", context={"login_form":form})
+
+
+@api_view()
+def post_list(request) :
+	if request.method =="GET":
+		posts = Post.objects.all()
+		serializer = PostSerializer(posts, many=True)
+		return Response(serializer.data)
+	if request.method == "POST":
+		serializer = PostSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view()
+def post_detail(request):
+    if request.method == "GET":
+        post = get_object_or_404(Post, pk=id)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    if request.method == "PUT":
+        post = get_object_or_404(Post, pk=id)
+        serializer = PostSerializer(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    if request.method == "DELETE":
+        post = get_object_or_404(Post, pk=id)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
