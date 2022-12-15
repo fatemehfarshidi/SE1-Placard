aed5d7a5847cfee601926776859de0862f1cbcb1
saya <sayahashemian@gmail.com>
2022-12-15 17:23:19 +0330

    admin updated

--- brick/admin.py
+++ brick/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
+
+
 
 # Register your models here.
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','user_id','type')
+    list_editable = ('type',)
+    ordering = ('title',)
+    search_fields = ('title','type')
